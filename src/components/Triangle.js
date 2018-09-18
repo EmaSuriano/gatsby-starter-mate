9a7467368630259997454cb276bf0150e9d849dd
EmaSuriano <emanuel.suriano@gmail.com>
2018-09-18 10:17:28 +0200

    Remove duplication in Triangle

--- src/components/Triangle.js
+++ src/components/Triangle.js
@@ -7,14 +7,13 @@ const Triangle = styled.div`
   z-index: -2;
 
   ${props => {
-    const border = `${props.height[0]} solid ${props.theme.colors[
-      props.color
-    ] || props.color};`;
+    const color = props.theme.colors[props.color] || props.color;
+    const border = `${props.height[0]} solid ${color};`;
     return props.invertY
       ? `border-bottom: ${border}; bottom: 0;`
       : `border-top: ${border};`;
   }}
-
+  /* prettier stop */
   ${props => {
     const border = `${props.width[0]} solid transparent;`;
     return props.invertX
@@ -24,18 +23,19 @@ const Triangle = styled.div`
 
 
   @media only screen and (min-width: 768px) {
-    ${props =>
-      props.invertY
-        ? `border-bottom-width: ${props.height[1]};`
-        : `border-top-width: ${props.height[1]};`}
-
-    /* stupid and sexy prettier stop breaking my styled-component */
-    
-    ${props =>
-      props.invertX
-        ? `border-left-width: ${props.width[1]};`
-        : `border-right-width: ${props.width[1]};`}
-    
+    ${props => {
+      const height = props.height[1];
+      return props.invertY
+        ? `border-bottom-width: ${height};`
+        : `border-top-width: ${height};`;
+    }}
+    /* prettier stop */
+    ${props => {
+      const width = props.width[1];
+      return props.invertX
+        ? `border-left-width: ${width};`
+        : `border-right-width: ${width};`;
+    }};
   }
 `;
 
