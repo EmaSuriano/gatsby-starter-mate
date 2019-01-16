ff4b6faec54a55813cd766c11f69b40ea01422ac
EmaSuriano <emanuel.suriano@gmail.com>
2019-01-16 15:56:32 -0300

    Revert changes in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,46 +9,42 @@ const client = contentful.createClient({
   accessToken: ACCESS_TOKEN,
 });
 
-const getAboutContentType = entry => entry.sys.contentType.sys.id === 'about';
-
 module.exports = client.getEntries().then(entries => {
-  const aboutEntry = entries.items.find(getAboutContentType);
+  const aboutEntry = entries.items.find(
+    entry => entry.sys.contentType.sys.id === 'about',
+  );
 
-  const plugins = [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: manifestConfig,
-    },
-    'gatsby-plugin-styled-components',
-    {
-      resolve: 'gatsby-plugin-google-fonts',
-      options: {
-        fonts: ['cabin', 'Open Sans'],
-      },
-    },
-    {
-      resolve: 'gatsby-source-contentful',
-      options: {
-        spaceId: SPACE_ID,
-        accessToken: ACCESS_TOKEN,
-      },
-    },
-    'gatsby-transformer-remark',
-    'gatsby-plugin-offline',
-  ];
-
-  // const { mediumUser } = aboutEntry.fields;
-  // if (mediumUser) {
-  plugins.push({
-    resolve: 'gatsby-source-medium',
-    options: {
-      username: aboutEntry.fields.mediumUser || '@medium',
-    },
-  });
-  // }
+  const { mediumUser = '@medium' } = aboutEntry.fields;
 
   return {
-    plugins,
+    plugins: [
+      'gatsby-plugin-react-helmet',
+      {
+        resolve: 'gatsby-plugin-manifest',
+        options: manifestConfig,
+      },
+      'gatsby-plugin-styled-components',
+      {
+        resolve: 'gatsby-plugin-google-fonts',
+        options: {
+          fonts: ['cabin', 'Open Sans'],
+        },
+      },
+      {
+        resolve: 'gatsby-source-contentful',
+        options: {
+          spaceId: SPACE_ID,
+          accessToken: ACCESS_TOKEN,
+        },
+      },
+      {
+        resolve: 'gatsby-source-medium',
+        options: {
+          username: mediumUser,
+        },
+      },
+      'gatsby-transformer-remark',
+      'gatsby-plugin-offline',
+    ],
   };
 });
