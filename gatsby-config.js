79081b6d52e8274533890d206cbde58982c8af1e
EmaSuriano <emanuel.suriano@gmail.com>
2019-01-22 17:41:06 -0300

    Make plugins dynamic based on env file

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,46 +11,50 @@ const client = contentful.createClient({
 
 const getAboutEntry = entry => entry.sys.contentType.sys.id === 'about';
 
+const plugins = [
+  'gatsby-plugin-react-helmet',
+  {
+    resolve: 'gatsby-plugin-manifest',
+    options: manifestConfig,
+  },
+  'gatsby-plugin-styled-components',
+  {
+    resolve: 'gatsby-plugin-google-fonts',
+    options: {
+      fonts: ['cabin', 'Open Sans'],
+    },
+  },
+  {
+    resolve: 'gatsby-source-contentful',
+    options: {
+      spaceId: SPACE_ID,
+      accessToken: ACCESS_TOKEN,
+    },
+  },
+  'gatsby-transformer-remark',
+  'gatsby-plugin-offline',
+];
+
 module.exports = client.getEntries().then(entries => {
   const { mediumUser = '@medium' } = entries.items.find(getAboutEntry).fields;
 
+  plugins.push({
+    resolve: 'gatsby-source-medium',
+    options: {
+      username: mediumUser,
+    },
+  });
+
+  if (ANALYTICS_ID) {
+    plugins.push({
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: ANALYTICS_ID,
+      },
+    });
+  }
+
   return {
-    plugins: [
-      'gatsby-plugin-react-helmet',
-      {
-        resolve: 'gatsby-plugin-manifest',
-        options: manifestConfig,
-      },
-      'gatsby-plugin-styled-components',
-      {
-        resolve: 'gatsby-plugin-google-fonts',
-        options: {
-          fonts: ['cabin', 'Open Sans'],
-        },
-      },
-      {
-        resolve: 'gatsby-source-contentful',
-        options: {
-          spaceId: SPACE_ID,
-          accessToken: ACCESS_TOKEN,
-        },
-      },
-      {
-        resolve: 'gatsby-source-medium',
-        options: {
-          username: mediumUser,
-        },
-      },
-      'gatsby-transformer-remark',
-      'gatsby-plugin-offline',
-      {
-        ...(ANALYTICS_ID && {
-          resolve: 'gatsby-plugin-google-analytics',
-          options: {
-            trackingId: ANALYTICS_ID,
-          },
-        }),
-      },
-    ],
+    plugins,
   };
 });
