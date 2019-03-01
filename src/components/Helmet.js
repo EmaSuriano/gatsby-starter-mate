db2e66023c5fb9efd851cad0de879c12ffec55ef
Onur Genes <onur@onurgenes.com>
2019-03-01 22:42:10 +0300

    Fixed twitter cards not showing images

--- src/components/Helmet.js
+++ src/components/Helmet.js
@@ -37,17 +37,17 @@ const Helmet = ({ theme = {} }) => (
           <meta charSet="utf-8" />
           <title>{title}</title>
           <meta name="description" content={description} />
-          <link rel="shortcut icon" href={profile.favicon32.src} />
+          <link rel="shortcut icon" href={`https:${profile.favicon32.src}`} />
           <meta name="theme-color" content={theme.background} />
-          <meta name="image" content={profile.favicon32.src} />
+          <meta name="image" content={`https:${profile.favicon32.src}`} />
 
           <meta itemProp="name" content={title} />
           <meta itemProp="description" content={description} />
-          <meta itemProp="image" content={profile.favicon32.src} />
+          <meta itemProp="image" content={`https:${profile.favicon32.src}`} />
 
           <meta name="og:title" content={title} />
           <meta name="og:description" content={description} />
-          <meta name="og:image" content={profile.bigIcon.src} />
+          <meta name="og:image" content={`https:${profile.bigIcon.src}`} />
           <meta name="og:site_name" content={title} />
           <meta name="og:locale" content="en_US" />
           <meta name="og:type" content="website" />
@@ -55,24 +55,25 @@ const Helmet = ({ theme = {} }) => (
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:title" content={title} />
           <meta name="twitter:description" content={description} />
-          <meta name="twitter:image" content={profile.bigIcon.src} />
+          <meta name="twitter:image" content={`https:${profile.bigIcon.src}`} />
+          <meta name="twitter:image:src" content={`https:${profile.bigIcon.src}`} />
 
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href={profile.appleIcon.src}
+            href={`https:${profile.appleIcon.src}`}
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href={profile.favicon32.src}
+            href={`https:${profile.favicon32.src}`}
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href={profile.favicon16.src}
+            href={`https:${profile.favicon16.src}`}
           />
 
           <link
