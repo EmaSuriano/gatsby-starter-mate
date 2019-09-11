91051ad4c5d74c9a42f860f613b229fd9c478a6c
EmaSuriano <emanuel.suriano@example.com>
2019-09-11 18:41:23 +0200

    adding TODO

--- patch-gatsby-source.js
+++ patch-gatsby-source.js
@@ -1,3 +1,4 @@
+// TODO: This patch is needed until this issue is solved --> https://github.com/gatsbyjs/gatsby/issues/17335
 const fs = require('fs');
 
 const FILE_PATH = './node_modules/gatsby-source-medium/gatsby-node.js';
