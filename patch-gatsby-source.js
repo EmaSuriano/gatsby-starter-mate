9b7b5e7f29f9e587fccb8ca6916a47aec54b3751
EmaSuriano <emanuel.suriano@example.com>
2019-09-12 09:40:07 +0200

    fix: patch medium-source using local dependency

--- patch-gatsby-source.js
+++
@@ -1,7 +0,0 @@
-// TODO: This patch is needed until this issue is solved --> https://github.com/gatsbyjs/gatsby/issues/17335
-const fs = require('fs');
-
-const FILE_PATH = './node_modules/gatsby-source-medium/gatsby-node.js';
-
-const content = fs.readFileSync(FILE_PATH, 'utf8');
-fs.writeFileSync(FILE_PATH, content.replace(/latest/g, ''), 'utf8');
