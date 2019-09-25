ad9e3d1c465597da569f9a93ecae9adb9629dd27
EmaSuriano <emanuel.suriano@gmail.com>
2019-09-25 11:05:34 +0200

    add wait in order to load fonts

--- cypress/e2e/visual-testing.test.js
+++ cypress/e2e/visual-testing.test.js
@@ -13,7 +13,7 @@ describe('Visual Testing', () => {
 
   it('404 Page', () => {
     cy.visit('/404');
-
+    cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
     cy.percySnapshot('404 Page Responsive', { widths: SCREENS });
   });
 });
