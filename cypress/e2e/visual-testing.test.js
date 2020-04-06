13a50c267af117a8c406bb09cfbf6cc0c2a4b63a
EmaSuriano <emanuel.suriano@gmail.com>
2020-04-06 20:18:57 +0200

    scrolling into views

--- cypress/e2e/visual-testing.test.js
+++ cypress/e2e/visual-testing.test.js
@@ -3,17 +3,23 @@
 describe('Visual Testing', () => {
   const SCREENS = [375, 768, 1280];
 
-  it('Main Page', () => {
-    cy.visit('/')
-      .scrollTo('bottom', { duration: 3000 })
-      .percySnapshot('Main Page Responsive', {
-        widths: SCREENS,
-      });
+  it('Main Page', async () => {
+    await cy.visit('/');
+
+    await cy.get('#home').scrollIntoView();
+    await cy.get('#about').scrollIntoView();
+    await cy.get('#projects').scrollIntoView();
+    await cy.get('#writing').scrollIntoView();
+    await cy.get('footer').scrollIntoView();
+
+    await cy.percySnapshot('Main Page Responsive', {
+      widths: SCREENS,
+    });
   });
 
-  it('404 Page', () => {
-    cy.visit('/404')
-      .wait(1000)
-      .percySnapshot('404 Page Responsive', { widths: SCREENS });
+  it('404 Page', async () => {
+    await cy.visit('/404');
+    await cy.wait(1000);
+    await cy.percySnapshot('404 Page Responsive', { widths: SCREENS });
   });
 });
