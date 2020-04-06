e3d6508e07e9c56c86dc381c114effac824cd69f
EmaSuriano <emanuel.suriano@gmail.com>
2020-04-06 15:25:45 +0200

    chain cypress methods

--- cypress/e2e/visual-testing.test.js
+++ cypress/e2e/visual-testing.test.js
@@ -1,22 +1,19 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
+
 describe('Visual Testing', () => {
   const SCREENS = [375, 768, 1280];
 
   it('Main Page', () => {
-    cy.visit('/');
-
-    cy.get('#home').scrollIntoView();
-    cy.get('#about').scrollIntoView();
-    cy.get('#projects').scrollIntoView();
-    cy.get('#writing').scrollIntoView();
-
-    cy.percySnapshot('Main Page Responsive', {
-      widths: SCREENS,
-    });
+    cy.visit('/')
+      .scrollTo('bottom', { duration: 3000 })
+      .percySnapshot('Main Page Responsive', {
+        widths: SCREENS,
+      });
   });
 
   it('404 Page', () => {
-    cy.visit('/404');
-    cy.wait(1000); // eslint-disable-line cypress/no-unnecessary-waiting
-    cy.percySnapshot('404 Page Responsive', { widths: SCREENS });
+    cy.visit('/404')
+      .wait(1000)
+      .percySnapshot('404 Page Responsive', { widths: SCREENS });
   });
 });
