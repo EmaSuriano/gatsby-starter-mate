51574adac39a167f20982ea599d73b12a8086492
EmaSuriano <emanuel.suriano@gmail.com>
2020-10-22 15:19:13 +0200

    move to media

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,12 +1,6 @@
-const { renameSync } = require('fs');
 const percyHealthCheck = require('@percy/cypress/task');
 
 module.exports = (on, config) => {
   on('task', percyHealthCheck);
-
-  on('after:screenshot', ({ path }) => {
-    renameSync(path, path.replace(/ \(\d*\)/i, ''));
-  });
-
   return config;
 };
